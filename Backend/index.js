014d2bc9e13973cb40c1d3ab8a3b0e9ab0042a67
abhaykawle2010-2010 <aabhhayy2010@gmail.com>
2025-01-03 17:51:33 +0530

    day1

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,6 @@
+require("dotenv").config();
 const express=require("express");
+const DBconnect = require("./src/config/connect");
 const PORT=process.env.PORT || 5000;
 const app=express();
 
@@ -9,7 +11,7 @@ app.get("/api",(req,res)=>{
 
 app.listen(PORT,async()=>{
     try {
-        await 
+        await DBconnect()
         console.log(`Server is running on port ${PORT}`);
     } catch (error) {
        console.log("Failed to connet connection"); 
