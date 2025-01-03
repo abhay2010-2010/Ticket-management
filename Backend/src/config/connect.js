cc5fe6ade2d7be1e335f48623926bf8dedad2bcf
abhaykawle2010-2010 <aabhhayy2010@gmail.com>
2025-01-03 18:57:26 +0530

    day1

--- Backend/src/config/connect.js
+++ Backend/src/config/connect.js
@@ -1,6 +1,6 @@
-require("dotenv").config();
+//require("dotenv").config();
 const mongoose=require("mongoose");
-const url=process.env.mongo_URL;
-const DBconnect=mongoose.connect("url");
+//const url=process.env.mongo_URL;
+const DBconnect=mongoose.connect("mongodb+srv://aabhhayy2010:rWQnKyU4UHpfM5Y4@cluster0.pxke5af.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
 module.exports=DBconnect;
